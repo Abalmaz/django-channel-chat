581b1e8d1164fd9b1d4567b15aefb4dddbd9c90b
Abalmaz <abalmazik@gmail.com>
2019-07-30 15:58:56 +0300

    Added handlers

--- chat/consumers.py
+++ chat/consumers.py
@@ -6,6 +6,13 @@ from chat.models import Room, ChatMessage
 
 
 class ChatConsumer(AsyncConsumer):
+
+    def __init__(self):
+        super().__init__()
+        self.room_obj = None
+        self.chat_room = None
+        self.msg = None
+
     async def websocket_connect(self, event):
         scope_room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_obj = await self.get_room(scope_room_name)
@@ -31,25 +38,17 @@ class ChatConsumer(AsyncConsumer):
 
     # Receive message from WebSocket
     async def websocket_receive(self, event):
-        print("receive", event)
+
+        handlers = {
+            'send': self.send_msg,
+        }
+
         message = event.get('text', None)
         text_data_json = json.loads(message)
-        msg = text_data_json.get('message')
-        user = self.scope['user']
-        username = user.username if user.is_authenticated else 'anonymous'
-
-        await self.create_chat_msg(user, msg)
-
-        await self.channel_layer.group_send(
-            self.chat_room,
-            {
-                "type": "chat_message",
-                "text": json.dumps({
-                    'message': msg,
-                    'username': username
-                })
-            }
-        )
+        self.msg = text_data_json.get('message', None)
+        command = text_data_json.get('command', None)
+        handler = handlers[command]
+        await handler()
 
     async def chat_message(self, event):
         await self.send({
@@ -62,7 +61,21 @@ class ChatConsumer(AsyncConsumer):
         return Room.objects.get_or_new(room_name)
 
     @database_sync_to_async
-    def create_chat_msg(self, user, msg):
+    def create_chat_msg(self, user):
         return ChatMessage.objects.create(room=self.room_obj,
                                           user=user,
-                                          message=msg)
+                                          message=self.msg)
+
+    async def send_msg(self):
+        user = self.scope['user']
+        await self.create_chat_msg(user)
+        await self.channel_layer.group_send(
+            self.chat_room,
+            {
+                "type": "chat_message",
+                "text": json.dumps({
+                    'message': self.msg,
+                    'username': user.username
+                })
+            }
+        )
